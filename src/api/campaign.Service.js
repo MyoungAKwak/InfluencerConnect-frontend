2684483322504add7bfbd2d7ff9b4f355721c037
MyoungAKwak <kyahddl@naver.com>
2025-12-07 15:50:04 +0900

    인플루언서 모집 캠페인 화면 구현 완료

---
+++ src/api/campaign.Service.js
@@ -0,0 +1,17 @@
+import axios from 'axios'
+
+// 프록시 key와 동일하게 '/api' 사용
+const api = axios.create({
+  baseURL: '/api',
+})
+
+export const getCampaign = (id) => api.get(`/campaign/${id}`)
+export const createCampaign = (formData) =>
+  api.post('/campaign', formData, {
+    headers: { 'Content-Type': 'multipart/form-data' },
+  })
+export const updateCampaign = (id, formData) =>
+  api.put(`/campaign/${id}`, formData, {
+    headers: { 'Content-Type': 'multipart/form-data' },
+  })
+export const deleteCampaign = (id) => api.delete(`/campaign/${id}`)
--- src/views/campaign/Edit.vue
+++ src/views/campaign/Edit.vue
@@ -4,32 +4,42 @@ import InputText from 'primevue/inputtext'
 import FileUpload from 'primevue/fileupload'
 import Button from 'primevue/button'
 import Dropdown from 'primevue/dropdown'
+import { useRouter, useRoute } from 'vue-router'
+import { createCampaign } from '@/api/campaign.Service'
+import { Form } from '@primevue/forms'
 
-// 상태
-const storeName = ref('')
-const phoneMiddle = ref('')
-const phoneLast = ref('')
-const mission = ref('') // ★ 추가됨
+const router = useRouter()
+const route = useRoute()
+
+const form = reactive({
+  storeName: '',
+  phoneMiddle: '',
+  phoneLast: '',
+  storeaddress: '',
+  content: '',
+  imageurl: '',
+  categoryId: '',
+})
 
 // 추가 상태
 const address = ref('')
 const category = ref(null)
 
 const categoryOptions = [
-  { label: '맛집', value: 'food' },
-  { label: '식품', value: 'grocery' },
-  { label: '뷰티', value: 'beauty' },
-  { label: '기타', value: 'etc' },
+  { label: '맛집', value: '1' },
+  { label: '식품', value: '2' },
+  { label: '뷰티', value: '3' },
+  { label: '기타', value: '4' },
 ]
 
-const errors = reactive<{
-  storeName?: string
-  file?: string
-  phone?: string
-  address?: string
-  category?: string
-  mission?: string
-}>({})
+// const errors = reactive<{
+//   storeName?: string
+//   file?: string
+//   phone?: string
+//   address?: string
+//   category?: string
+//   mission?: string
+// }>({})
 
 //다음 주소검색 api
 function openAddressSearch() {
@@ -42,45 +52,63 @@ function openAddressSearch() {
   }).open()
 }
 
-function handleFileUpload(event: any) {
-  const file = event.files?.[0]
-  if (!file) return
-  errors.file = ''
-  if (!file.type.startsWith('image/')) errors.file = '이미지 파일만 업로드 가능'
-  if (file.size > 5 * 1024 * 1024) errors.file = '5MB 이하만 업로드 가능'
+// function handleFileUpload(event: any) {
+//   const file = event.files?.[0]
+//   if (!file) return
+//   errors.file = ''
+//   if (!file.type.startsWith('image/')) errors.file = '이미지 파일만 업로드 가능'
+// }
+
+// function validate() {
+//   errors.storeName = ''
+//   errors.phone = ''
+//   errors.address = ''
+//   errors.category = ''
+//   errors.mission = ''
+
+//   if (!storeName.value.trim()) errors.storeName = '상호명을 입력해주세요'
+//   if (!/^[0-9]{3,4}$/.test(phoneMiddle.value) || !/^[0-9]{4}$/.test(phoneLast.value)) {
+//     errors.phone = '올바른 연락처 입력'
+//   }
+//   if (!address.value.trim()) errors.address = '주소를 입력해주세요'
+//   if (!category.value) errors.category = '카테고리를 선택해주세요'
+//   if (!mission.value.trim()) errors.mission = '체험단 미션을 입력해주세요'
+
+//   return !(errors.storeName || errors.phone || errors.address || errors.category || errors.mission)
+// }
+
+const uploadFile = (event) => {
+  form.imageurl = event.files[0]
 }
 
-function validate() {
-  errors.storeName = ''
-  errors.phone = ''
-  errors.address = ''
-  errors.category = ''
-  errors.mission = ''
-
-  if (!storeName.value.trim()) errors.storeName = '상호명을 입력해주세요'
-  if (!/^[0-9]{3,4}$/.test(phoneMiddle.value) || !/^[0-9]{4}$/.test(phoneLast.value)) {
-    errors.phone = '올바른 연락처 입력'
+// 저장 클릭
+const saveCampaign = async () => {
+  try {
+    // FormData 생성
+    const payload = new FormData()
+    payload.append('storeName', form.storeName)
+    payload.append('phone', `010-${form.phoneMiddle}-${form.phoneLast}`)
+    payload.append('storeAddress', address.value)
+    payload.append('categoryId', form.categoryId)
+    payload.append('content', form.content)
+    if (form.imageurl) payload.append('imageUrl', form.imageurl)
+    console.log('form.imageurl', form.imageurl)
+    await createCampaign(payload)
+    router.push('/campaign/list')
+  } catch (error) {
+    console.error('게시글 저장 실패:', error)
   }
-  if (!address.value.trim()) errors.address = '주소를 입력해주세요'
-  if (!category.value) errors.category = '카테고리를 선택해주세요'
-  if (!mission.value.trim()) errors.mission = '체험단 미션을 입력해주세요'
-
-  return !(errors.storeName || errors.phone || errors.address || errors.category || errors.mission)
 }
 
-function submitForm() {
-  if (!validate()) return
-
-  const payload = {
-    storeName: storeName.value,
-    phone: `010-${phoneMiddle.value}-${phoneLast.value}`,
-    address: address.value,
-    category: category.value,
-    mission: mission.value,
-  }
-  console.log('submit payload', payload)
-  alert('저장되었습니다.')
-}
+// const payload = {
+//   storeName: form.storeName,
+//   phone: `010-${form.phoneMiddle}-${form.phoneLast}`,
+//   address: address.value,
+//   category: category.value,
+//   content: form.content,
+// }
+// console.log('submit payload', payload)
+// alert('저장되었습니다.')
 </script>
 
 <template>
@@ -88,12 +116,15 @@ function submitForm() {
     <div class="p-card p-6 w-full max-w-2xl space-y-8">
       <h1 class="text-2xl font-bold mb-4">기본정보</h1>
 
-      <form @submit.prevent="submitForm" class="space-y-8">
+      <form class="space-y-8">
         <!-- 상호명 -->
         <div class="space-y-2">
           <label class="text-lg font-bold">상호명</label>
-          <InputText v-model="storeName" placeholder="상호명을 작성해 주세요." class="w-full" />
-          <small v-if="errors.storeName" class="p-error">{{ errors.storeName }}</small>
+          <InputText
+            v-model="form.storeName"
+            placeholder="상호명을 작성해 주세요."
+            class="w-full"
+          />
         </div>
 
         <!-- 썸네일 등록 -->
@@ -107,12 +138,11 @@ function submitForm() {
             chooseLabel="파일 선택"
             uploadLabel="업로드"
             cancelLabel="취소"
-            @select="handleFileUpload"
+            @select="uploadFile"
           />
           <small class="text-sm block text-gray-600">
             검수 불가 예시: 간판/로고/화면캡처/텍스트가 큰 이미지
           </small>
-          <small v-if="errors.file" class="p-error">{{ errors.file }}</small>
         </div>
 
         <!-- 담당자 연락처 -->
@@ -122,20 +152,20 @@ function submitForm() {
             <span class="font-bold">010</span>
             <span>-</span>
             <InputText
-              v-model="phoneMiddle"
+              v-model="form.phoneMiddle"
               maxlength="4"
               placeholder="숫자 입력"
               style="width: 110px"
             />
             <span>-</span>
             <InputText
-              v-model="phoneLast"
+              v-model="form.phoneLast"
               maxlength="4"
               placeholder="숫자 입력"
               style="width: 110px"
             />
           </div>
-          <small v-if="errors.phone" class="p-error">{{ errors.phone }}</small>
+          <!-- <small v-if="errors.phone" class="p-error">{{ errors.phone }}</small> -->
         </div>
 
         <!-- 주소 -->
@@ -149,21 +179,21 @@ function submitForm() {
               @click.prevent="openAddressSearch"
             />
           </div>
-          <small v-if="errors.address" class="p-error">{{ errors.address }}</small>
+          <!-- <small v-if="errors.address" class="p-error">{{ errors.address }}</small> -->
         </div>
 
         <!-- 카테고리 선택 -->
         <div class="space-y-2">
           <label class="text-lg font-bold">카테고리</label>
           <Dropdown
-            v-model="category"
+            v-model="form.categoryId"
             :options="categoryOptions"
             optionLabel="label"
             optionValue="value"
             placeholder="카테고리를 선택하세요"
             class="w-full"
           />
-          <small v-if="errors.category" class="p-error">{{ errors.category }}</small>
+          <!-- <small v-if="errors.category" class="p-error">{{ errors.category }}</small> -->
         </div>
 
         <!-- 체험단 미션 -->
@@ -177,18 +207,18 @@ function submitForm() {
           </small>
 
           <textarea
-            v-model="mission"
+            v-model="form.content"
             class="p-inputtext p-inputtext-lg w-full border rounded-lg p-3"
             rows="6"
             placeholder="체험단 미션을 작성해주세요"
           ></textarea>
 
-          <small v-if="errors.mission" class="p-error">{{ errors.mission }}</small>
+          <!-- <small v-if="errors.mission" class="p-error">{{ errors.mission }}</small> -->
         </div>
 
         <!-- 저장 버튼 -->
         <div class="flex justify-end pt-2">
-          <Button label="저장" class="p-button-primary px-6" />
+          <Button label="저장" @click="saveCampaign" class="p-button-primary px-6" />
         </div>
       </form>
     </div>
